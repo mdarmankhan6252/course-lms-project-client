29c6d1d95eab7ad33017ceabed88b41cd44d0395
mdarmankhan6252 <mdarmankhan6252@gmail.com>
2025-02-09 15:03:01 +0600

    I have created the companies page

--- src/pages/student/Home.jsx
+++ src/pages/student/Home.jsx
@@ -1,9 +1,11 @@
+import Companies from "../../components/student/Companies";
 import Hero from "../../components/student/Hero";
 
 const Home = () => {
    return (
       <div className="flex flex-col items-center justify-center space-y-7 text-center">
          <Hero />
+         <Companies />
 
       </div>
    );
